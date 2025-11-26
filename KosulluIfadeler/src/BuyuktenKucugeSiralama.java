0ac9a6f93319c11631e65005f2eb8ff3d32f0ac3
Muhammed Esad Karanfil <karanfilesad28@gmail.com>
2025-11-26 14:03:21 +0300

    Büyükten küçüğe Sıralama

--- KosulluIfadeler/src/BuyuktenKucugeSiralama.java
+++ KosulluIfadeler/src/BuyuktenKucugeSiralama.java
@@ -6,6 +6,35 @@ public class BuyuktenKucugeSiralama {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("");
+        System.out.print("1. Sayıyı giriniz : ");
+        a = input.nextInt();
+
+        System.out.print("2. Sayıyı giriniz : ");
+        b = input.nextInt();
+
+        System.out.print("3. Sayıyı giriniz : ");
+        c = input.nextInt();
+
+        if (a > b && a > c) {
+            if (b > c) {
+                System.out.print("a > b > c");
+            } else {
+                System.out.print("a > c > b");
+            }
+        } else if (b > a && c > a) {
+            if (b > c) {
+                System.out.print("b > c > a");
+            } else {
+                System.out.print("c > b > a");
+            }
+        }
+        else {
+            if (a > b) {
+                System.out.print("c > a > b");
+            }
+            else {
+                System.out.print("b > a > c");
+            }
+        }
     }
 }
